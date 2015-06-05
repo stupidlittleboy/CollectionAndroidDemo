d56f7cfc410ecca2e59d7e5ff6c102d4e7e426ae
stupidlittleboy <1425211552@qq.com>
2015-06-05 14:49:26 +0800

    2048修改

--- app/src/main/java/com/stupidman/admin/collectionandroiddemo/game2048/Game2048Configuration.java
+++ app/src/main/java/com/stupidman/admin/collectionandroiddemo/game2048/Game2048Configuration.java
@@ -1,40 +1,77 @@
 package com.stupidman.admin.collectionandroiddemo.game2048;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import com.stupidman.admin.collectionandroiddemo.R;
 
-public class Game2048Configuration extends ActionBarActivity {
+public class Game2048Configuration extends ActionBarActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener {
+
+    private Spinner spinnerGameLevel;
+    private Spinner spinnerGameGoal;
+    private Button btnGameBack;
+    private Button btnGAmeDone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game2048_configuration);
+
+        init();
     }
 
+    private void init() {
+        spinnerGameLevel = (Spinner) findViewById(R.id.spinner_game_level);
+        spinnerGameGoal = (Spinner) findViewById(R.id.spinner_game_goal);
+        btnGameBack = (Button) findViewById(R.id.btn_game2048_back);
+        btnGAmeDone = (Button) findViewById(R.id.btn_game2048_done);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_game2048_configuration, menu);
-        return true;
+        spinnerGameLevel.setOnItemSelectedListener(this);
+        spinnerGameGoal.setOnItemSelectedListener(this);
+        btnGameBack.setOnClickListener(this);
+        btnGAmeDone.setOnClickListener(this);
+    }
+
+    private void saveConfig() {
+        SharedPreferences sp = getSharedPreferences("game2048Configuration", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putInt("game_line", Integer.parseInt(spinnerGameLevel.getSelectedItem().toString()));
+        editor.putInt("game_goal", Integer.parseInt(spinnerGameGoal.getSelectedItem().toString()));
+        editor.commit();
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.btn_game2048_back:
+                this.finish();
+                break;
+
+            case R.id.btn_game2048_done:
+                saveConfig();
+                setResult(RESULT_OK);
+                this.finish();
+                break;
+            default:
+                break;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
